950fc39ae86e9576c524a4f288ba91220e1cffcd
Thelgaris <alex.gabarron84@gmail.com>
2022-05-02 17:09:42 +0000

    Commit

--- src/models.py
+++ src/models.py
@@ -4,9 +4,26 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(120), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+class Characters(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    characters_list = db.Column(db.String(120), unique=True, nullable=False)
+
+class One_character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    character_name = db.Column(db.String(120), unique=True, nullable=False)
+    character_props = db.Column(db.String(120), unique=True, nullable=False)
+    
+
+class Favourites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+
 
     def __repr__(self):
         return '<User %r>' % self.username
